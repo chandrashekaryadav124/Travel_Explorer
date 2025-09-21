cdfa28f5fb6d213c7ba0f85bee7711b9f656d755
chandrashekar <chandrashekarchandu490@gmail.com>
2025-09-21 15:57:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import PrivateRoute from "./routes/PrivateRoute";
 import Navbar from "./component/Navbar";
-import Footer from "./component/footer";
+import Footer from "./component/Footer";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
